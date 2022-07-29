6fa44964b9d3a5cddf78b12fad9bc071e993b211
Isaac Vander Sluis <isaacvandersluis@gmail.com>
2022-07-29 16:37:59 +0100

    Add ComputeJacobian function

--- src/tools.cpp
+++ src/tools.cpp
@@ -38,8 +38,24 @@ VectorXd Tools::CalculateRMSE(
 
 MatrixXd Tools::CalculateJacobian(const VectorXd & x_state)
 {
-  /**
-   * TODO:
-   * Calculate a Jacobian here.
-   */
+  MatrixXd Hj(3, 4);
+  // recover state parameters
+  float px = x_state(0);
+  float py = x_state(1);
+  float vx = x_state(2);
+  float vy = x_state(3);
+
+  // check division by zero
+  if (px + py == 0) return Hj;
+
+  // compute denominators in advance for efficiency
+  float d1 = px * px + py * py;
+  float d2 = sqrt(d1);
+  float d3 = (d1 * d2);
+
+  // compute the Jacobian matrix
+  Hj << px / d2, py / d2, 0, 0, -py / d1, px / d1, 0, 0, py * (vx * py - vy * px) / d3,
+    px * (vy * px - vx * py) / d3, px / d2, py / d2;
+
+  return Hj;
 }
