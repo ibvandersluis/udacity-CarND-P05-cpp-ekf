fb97cb3b0318c7afc7a2c100b94f796c0245b718
Isaac Vander Sluis <isaacvandersluis@gmail.com>
2022-07-29 16:34:49 +0100

    Add CalculateRMSE function

--- src/tools.cpp
+++ src/tools.cpp
@@ -13,9 +13,27 @@ Tools::~Tools() {}
 VectorXd Tools::CalculateRMSE(
   const vector<VectorXd> & estimations, const vector<VectorXd> & ground_truth)
 {
-  /**
-   * TODO: Calculate the RMSE here.
-   */
+  VectorXd rmse(4);
+  rmse << 0, 0, 0, 0;
+
+  if (estimations.size() == 0 || estimations.size() != ground_truth.size()) {
+    return rmse;
+  }
+
+  // Accumulate squared residuals
+  for (int i = 0; i < estimations.size(); ++i) {
+    VectorXd residual = estimations[i] - ground_truth[i];
+    residual = residual.array() * residual.array();
+    rmse += residual;
+  }
+
+  // Calculate the mean
+  rmse = rmse / estimations.size();
+
+  // Calculate the squared root
+  rmse = rmse.array().sqrt();
+
+  return rmse;
 }
 
 MatrixXd Tools::CalculateJacobian(const VectorXd & x_state)
