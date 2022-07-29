7aed7d7458fe8aeb9f3f89bc8de08ed2c0da7c36
Isaac Vander Sluis <isaacvandersluis@gmail.com>
2022-07-29 16:29:57 +0100

    Add FusionEKF header and src files

---
+++ include/kalman_filter/fusion_ekf.hpp
@@ -0,0 +1,51 @@
+#ifndef KALMAN_FILTER__FUSION_EKF_HPP_
+#define KALMAN_FILTER__FUSION_EKF_HPP_
+
+#include <fstream>
+#include <string>
+#include <vector>
+
+#include "Eigen/Dense"
+#include "kalman_filter.hpp"
+#include "measurement_package.hpp"
+#include "tools.hpp"
+
+class FusionEKF
+{
+public:
+  /**
+   * Constructor.
+   */
+  FusionEKF();
+
+  /**
+   * Destructor.
+   */
+  virtual ~FusionEKF();
+
+  /**
+   * Run the whole flow of the Kalman Filter from here.
+   */
+  void ProcessMeasurement(const MeasurementPackage & measurement_pack);
+
+  /**
+   * Kalman Filter update and prediction math lives in here.
+   */
+  KalmanFilter ekf_;
+
+private:
+  // check whether the tracking toolbox was initialized or not (first measurement)
+  bool is_initialized_;
+
+  // previous timestamp
+  long long previous_timestamp_;
+
+  // tool object used to compute Jacobian and RMSE
+  Tools tools;
+  Eigen::MatrixXd R_laser_;
+  Eigen::MatrixXd R_radar_;
+  Eigen::MatrixXd H_laser_;
+  Eigen::MatrixXd Hj_;
+};
+
+#endif  // KALMAN_FILTER__FUSION_EKF_HPP_
