72d907b4858e563490530b4a20bd1055f4bb9f22
Isaac Vander Sluis <isaacvandersluis@gmail.com>
2022-08-01 16:44:29 +0100

    Remove unnecessary namespaces

--- include/kalman_filter/kalman_filter.hpp
+++ include/kalman_filter/kalman_filter.hpp
@@ -29,8 +29,8 @@ public:
    * @param Q_in Process covariance matrix
    */
   void Init(
-    Eigen::VectorXd & x_in, Eigen::MatrixXd & P_in, Eigen::MatrixXd & F_in, Eigen::MatrixXd & H_in,
-    Eigen::MatrixXd & R_in, Eigen::MatrixXd & Q_in);
+    VectorXd & x_in, MatrixXd & P_in, MatrixXd & F_in, MatrixXd & H_in, MatrixXd & R_in,
+    MatrixXd & Q_in);
 
   /**
    * Predict Predicts the state and the state covariance
@@ -48,7 +48,7 @@ public:
    * Updates the state by using Extended Kalman Filter equations
    * @param z The measurement at k+1
    */
-  void UpdateEKF(const Eigen::VectorXd & z);
+  void UpdateEKF(const VectorXd & z);
 
   // state vector
   VectorXd x_;
